5b3ffe3672c91abd2d08e5051c9458375d0d09dd
Cauan <cauangerlach297@gmail.com>
2024-03-14 15:29:09 -0300

    inserir a regex

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ import fs from "fs";
 import chalk from 'chalk';
 
 
+
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks (texto){
+    const regex = /\[[^[[\]*?\]\(https?:\/\/[^\s?#.]*[^\s]*\)/gm;
+
+    const capturas=texto.match(regex);
+
+    console.log(capturas);
+}
+
+extraiLinks(textoTeste);
+
 // console.log(chalk.blue('Olá mundo'));
 
 // console.log(chalk.blue.bgWhite.bold("SENAI"));
@@ -18,9 +31,9 @@ import chalk from 'chalk';
 // `);
 
 //2)tratamento de erros, função para tratar os erros
-function trataErro(erro) {
-    throw new Error(chalk.red(erro.code, "não há arquivo no diretório"));
-}
+// function trataErro(erro) {
+//     throw new Error(chalk.red(erro.code, "não há arquivo no diretório"));
+// }
 
 // //1) escever uma function que traga arquivos .md
 // function pegaArquivo(caminhoDoArquivo){
@@ -49,18 +62,18 @@ function trataErro(erro) {
 
 //-----------usando outra forma de solucionar as promessas (async/await)
 
-async function pegaArquivo(caminhoDoArquivo) {
-    try {
-        const encoding = "utf-8";
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.cyan(texto));
-    }catch (erro){
-        trataErro(erro);
-    }finally{
-        console.log(chalk.magenta('operação concluida'))
-    }
+// async function pegaArquivo(caminhoDoArquivo) {
+//     try {
+//         const encoding = "utf-8";
+//         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+//         console.log(chalk.cyan(texto));
+//     }catch (erro){
+//         trataErro(erro);
+//     }finally{
+//         console.log(chalk.magenta('operação concluida'))
+//     }
       
-}
+// }
 
-//pegaArquivo('./arquivos/');
-pegaArquivo('./arquivos/texto.md');
+// //pegaArquivo('./arquivos/');
+// pegaArquivo('./arquivos/texto.md');
